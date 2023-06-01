5d1a1d092e7ab9d4ebb2db35c972977d5a42b7e8
Jonathan-Pender <jonathan.pender.dev@gmail.com>
2023-05-31 22:12:36 -0300

    entregando el parcial

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import Card from "./components/Card";
+import "./App.css";
+
+function App() {
+  //useState
+  const [hincha, setHincha] = useState("");
+  const [club, setClub] = useState("");
+  const [bandera, setBandera] = useState(false);
+
+  //handlers
+  const onChangeHincha = (e) => setHincha(e.target.value);
+  const onChangeClub = (e) => setClub(e.target.value);
+
+  const validandoHincha = (hincha) => {
+    const sinEspacios = hincha.trim();
+
+    if (sinEspacios.length < 3) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const validandoClub = (club) => {
+    const sinEspacios = club.trim();
+
+    if (sinEspacios.length < 6) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+
+    const isHinchaValido = validandoHincha(hincha);
+    const isClubValido = validandoClub(club);
+
+    if (!isHinchaValido || !isClubValido) {
+      alert("“Por favor chequea que la información sea correcta”");
+    } else {
+      alert(`Bienvenido: ${hincha} al club mas grande ${club}`);
+      setBandera(true);
+    }
+  };
+
+  return (
+    <>
+      <h1>Futbol de primera</h1>
+      <div className="container-formulario">
+        <h4>Ingrese su nombre y club favorito</h4>
+
+        <form action="" onSubmit={onSubmitForm}>
+          <input
+            type="text"
+            placeholder="Nombre del hincha"
+            value={hincha}
+            onChange={onChangeHincha}
+          />
+
+          <input
+            type="text"
+            value={club}
+            placeholder="club favorito"
+            onChange={onChangeClub}
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+      <div className="container card">
+        {bandera && <Card hincha={hincha} club={club} />}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = ({ hincha, club }) => {
+  return (
+    <>
+      <h1>Hola {hincha}</h1>
+      <h3>Bienvenido al club mais grande do mundo {club}</h3>
+    </>
+  );
+};
+
+export default Card;
